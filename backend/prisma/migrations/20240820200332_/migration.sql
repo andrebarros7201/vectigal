482908f2c50a078b5007214fe32e1f552bc914c0
Andre Barros <andrebarros7201@gmail.com>
2024-08-20 21:04:32 +0100

    fix prisma errors

--- backend/prisma/migrations/20240820200332_/migration.sql
+++ backend/prisma/migrations/20240820200332_/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );

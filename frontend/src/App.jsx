ec0a59fe5d8a5181af4cc2791188367793ce825e
Andre Barros <andrebarros7201@gmail.com>
2024-08-23 06:14:57 +0100

    Create context and pass states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import { Outlet } from "react-router-dom";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext(null);
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErrors] = useState(null);
+
   return (
     <>
-      <Header />
-      <Outlet />
+      <AppContext.Provider
+        value={{
+          isLoggedIn,
+          setIsLoggedIn,
+          user,
+          setUser,
+          isLoading,
+          setIsLoading,
+          errors,
+          setErrors,
+        }}
+      >
+        <Header />
+        <Outlet />
+      </AppContext.Provider>
     </>
   );
 }

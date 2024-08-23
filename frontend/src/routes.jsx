c442a7b09d6f65b79d70304b9bf274a9e8872f2d
Andre Barros <andrebarros7201@gmail.com>
2024-08-23 06:11:50 +0100

    update routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,12 +1,24 @@
 import App from "./App.jsx";
 import Home from "./components/Home/Home.jsx";
 import { createBrowserRouter } from "react-router-dom";
+import LogIn from "./components/Log In/LogIn.jsx";
+import SignUp from "./components/Sign Up/SignUp.jsx";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/log-in",
+        element: <LogIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+    ],
   },
 ]);
 
